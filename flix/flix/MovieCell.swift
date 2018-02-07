e18804fce3985aaa54a1a8b35a6103dbed79a916
Angel Mitchell <angel.mitchell@bison.howard.edu>
2018-02-07 00:26:30 -0500

    TlbView + Data

---
+++ flix/flix/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Angel Chara'e Mitchell on 2/7/18.
+//  Copyright © 2018 Angel Chara'e Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- flix/flix/Now PlayingViewController.swift
+++ flix/flix/Now PlayingViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class Now_PlayingViewController: UIViewController {
+class Now_PlayingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    var movies: [[String : Any]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -22,17 +27,36 @@ class Now_PlayingViewController: UIViewController {
             if let error = error {
                 print(error.localizedDescription)
             } else if let data = data {
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                print(dataDictionary)
-                // TODO: Get the array of movies
-                // TODO: Store the movies in a property to use elsewhere
-                // TODO: Reload your table view data
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: [])
+                    as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String : Any]]
+                self.movies = movies
+                    
+                //Reload your table view data
+                self.tableView.reloadData()
                 
             }
         }
         task.resume()
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        return cell
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
