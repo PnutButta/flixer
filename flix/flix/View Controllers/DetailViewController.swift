5059c4ff24bbd260601e3c8edb1b184aa3da1cdc
Angel Mitchell <angel.mitchell@bison.howard.edu>
2018-02-17 18:18:51 -0500

    new view controller

---
+++ flix/flix/View Controllers/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  flix
+//
+//  Created by Angel Chara'e Mitchell on 2/17/18.
+//  Copyright © 2018 Angel Chara'e Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
