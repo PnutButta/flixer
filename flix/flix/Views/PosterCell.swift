26548b38d82c6a1930db0b9096b34ac8b4447885
Angel Mitchell <angel.mitchell@bison.howard.edu>
2018-02-26 16:24:37 -0500

    loaded collectionView

---
+++ flix/flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Angel Chara'e Mitchell on 2/26/18.
+//  Copyright © 2018 Angel Chara'e Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
