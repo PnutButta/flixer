0072c119cc32ef5c6414bb305e0b11c49ef4eab7
Angel Mitchell <angel.mitchell@bison.howard.edu>
2018-03-25 13:01:00 -0400

    Milestone 4

--- flix/flix/View Controllers/Now PlayingViewController.swift
+++ flix/flix/View Controllers/Now PlayingViewController.swift
@@ -48,32 +48,18 @@ class Now_PlayingViewController: UIViewController, UITableViewDataSource, UITabl
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
         
         cell.movie = movies[indexPath.row]
-        
         return cell
     }
     
     func fetchMovies() {
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-            // This will run when the network request returns
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data {
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: [])
-                    as! [String: Any]
-                let movieDictionaries = dataDictionary["results"] as! [[String : Any]]
-                self.movies = Movie.movies(dictionaries: movieDictionaries)
-                
-                //Reload your table view data
+        MovieApiManager().popularMovies { (movies: [Movie]?, error: Error?) in
+            if let movies = movies {
+                self.movies = movies
                 self.tableView.reloadData()
                 self.refresh.endRefreshing()
             }
         }
-        task.resume()
     }
-   
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let cell =  sender as! UITableViewCell
--- flix/flix/View Controllers/SuperheroViewController.swift
+++ flix/flix/View Controllers/SuperheroViewController.swift
@@ -61,25 +61,13 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource {
     }
     
     func fetchMovies() {
-        let url = URL(string: "https://api.themoviedb.org/3/movie/284054/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-            // This will run when the network request returns
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data {
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: [])
-                    as! [String: Any]
-                let movieDictionaries = dataDictionary["results"] as! [[String : Any]]
-                self.movies = Movie.movies(dictionaries: movieDictionaries)
-                
-                //Reload your table view data
+        MovieApiManager().superheroMovies { (movies: [Movie]?, error: Error?) in
+            if let movies = movies {
+                self.movies = movies
                 self.collectionView.reloadData()
                 self.refresh.endRefreshing()
             }
         }
-        task.resume()
     }
     
     override func didReceiveMemoryWarning() {
